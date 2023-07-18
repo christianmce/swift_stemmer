d2dbaf5509274e95842c84d7eb5090c1a5208a58
ChristianMCE <cmce@unach.mx>
2023-07-18 12:07:12 -0600

    Update stemmer.swift

--- stemmer.swift
+++ stemmer.swift
@@ -3,17 +3,42 @@
 // Filter for perferming word stemming based on the Porter algorithm
 // Copyright (c) 2023 Christian Mauricio Castillo Estrada (cmce at unach.mx)
 // **************************************************************************
+
+// This is the Porter stemming algorithm, ported to Python from the version coded up in ANSI C by the author. It may be be regarded
+// as canonical, in that it follows the algorithm presented in Porter, 1980, An algorithm for suffix stripping, Program, Vol. 14,
+// no. 3, pp 130-137, only differing from it at the points maked --DEPARTURE-- below.
+// See also http://www.tartarus.org/~martin/PorterStemmer
+// **************************************************************************
+// The algorithm as described in the paper could be exactly replicated by adjusting the points of DEPARTURE, but this is barely necessary,
+// because (a) the points of DEPARTURE are definitely improvements, and (b) no encoding of the Porter stemmer I have seen is anything like
+// as exact as this version, even with the points of DEPARTURE!
+
 import UIKit
 
 class Stemmer {
-
+  private var b = [Character]()
   // offset into b 
   private var i: Int
   // offset to end of stemmed word */
   private var i_end, k, k: Int
   // unit of size whereby b is increased 
   private let INC: Int = 50
-                       
+
+  func add(_ ch: Character) {
+    if (i == b.count)
+    {
+      var new_b = new Character[i+INC]
+      var c: Int = 0
+        while(c<i){
+          new_b[c] = b[c]
+        }
+        b = new_b
+    }
+    b[i++] = ch
+  }
+   
+  
+  //***************************************************************** avances **********************
   var toDoCount: Int { return toDoItems.count }
   var doneCount: Int { return doneItems.count }
   private var doneItems = [ToDoItem]()
