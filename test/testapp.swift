6ad6231abf3410f381f5d7a6994ce0f8d212013d
ChristianMCE <cmce@unach.mx>
2025-11-23 17:19:11 -0600

    Update testapp.swift

--- test/testapp.swift
+++ test/testapp.swift
@@ -1,1 +1,18 @@
+import Foundation
 
+func testmain() {
+    
+    let inputHandle = FileHandle.standardInput
+    let outputHandle = FileHandle.standardOutput
+        
+    while let line = readLine() {
+        
+        let stemmedWord = stem(line)
+                
+        if let data = (stemmedWord + "\n").data(using: .utf8) {
+            outputHandle.write(data)
+        }
+    }
+}
+
+testmain()
