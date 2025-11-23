9d3ad84656c4fe11b0b5385668db8181031bea46
ChristianMCE <cmce@unach.mx>
2025-11-23 17:24:59 -0600

    Update testapp.swift

--- test/testapp.swift
+++ test/testapp.swift
@@ -1,12 +1,29 @@
+/// *******************************************************************************
+/// FPorterStemmer.swift - November 2025
+/// Filter for perferming word stemming based on the Porter algorithm
+/// Copyright (c) 2025 Christian Mauricio Castillo Estrada (cmce at unach.mx)
+/// *******************************************************************************
+
 import Foundation
 
 func testmain() {
-    
-    let inputHandle = FileHandle.standardInput
+    let fileName = "in.txt"    
+    let currentDirectory = FileManager.default.currentDirectoryPath
+    let filePath = "\(currentDirectory)/\(fileName)"
     let outputHandle = FileHandle.standardOutput
+    
+    guard FileManager.default.fileExists(atPath: filePath) else {
+        print("Error: The file '\(fileName)' does'nt exist in \(currentDirectory)")
+        return
+    }
+    
+    do {
         
-    while let line = readLine() {
+        let content = try String(contentsOfFile: filePath, encoding: .utf8)
+        let lines = content.components(separatedBy: .newlines)
         
+        for (index, line) in lines.enumerated() {    
+       
         let stemmedWord = stem(line)
                 
         if let data = (stemmedWord + "\n").data(using: .utf8) {
