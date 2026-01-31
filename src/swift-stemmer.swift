0cb82ba2cf28786305daae89c1b34660f37cce69
ChristianMCE <cmce@unach.mx>
2026-01-31 00:06:37 -0600

    Update swift-stemmer.swift

--- src/swift-stemmer.swift
+++ src/swift-stemmer.swift
@@ -405,3 +405,44 @@ func stem(_ word: String) -> Result<String, StemmerError> {
     }
 }
 
+
+// MARK: ---------------------------------------------------------------------------------------------------------------------------------
+
+// Main Example: I have included Dr Porter's sample input and output text in a test suite (input.txt)
+// Declare the URLs of the input and output files
+    let urlInput = URL(fileURLWithPath: "input.txt")
+    let urlOutput = URL(fileURLWithPath: "outputdata.txt")
+    print("\n Swift Stemmer Output: \n")
+
+    let Buffercapacity = 23700
+    var entryLines: [String] = []
+    var outputLines: [String] = []
+    entryLines.reserveCapacity(Buffercapacity)
+    outputLines.reserveCapacity(Buffercapacity)
+
+    // Read file input.txt ---------------------------------------------------------------
+    do {
+        let contentBody = try String(contentsOf: urlInput, encoding: .utf8)
+        entryLines = contentBody.components(separatedBy: .newlines)        
+        print(" \(entryLines.count) input words were read")
+
+        for rowword in entryLines {
+            if case .success(let stemmed) = stem(rowword) {
+                outputLines.append(stemmed)
+            }      
+        }
+        
+    } catch {
+        print("Error reading file: \(error)")        
+    }
+    
+    // Write file output  ----------------------------------------------------------------
+    do {
+        let contentBodyOut = outputLines.joined(separator: "\n")
+        try contentBodyOut.write(to: urlOutput, atomically: true, encoding: .utf8)        
+        print("File saved successfully")
+        
+    } catch {
+        print("Error writing to file: \(error)")
+    }
+
